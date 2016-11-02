d10fc574bad7d3d6ee69454866e8bf8d6eaeca21
peterittner <peter.ittner@gmail.com>
2016-11-02 23:16:58 +0100

    initial commit

---
+++ BingDesktop/Program.cs
@@ -0,0 +1,83 @@
+﻿using BingDesktop.API;
+using BingDesktop.Data;
+using BingDesktop.Misc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingDesktop
+{
+    class Program
+    {
+        private const string ENDPOINT = @"/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=de-De";
+        private const string HOST = @"http://www.bing.com";
+        private const string EXT = @".jpg";
+
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                await MainInternal(args);
+            }).Wait();
+            Console.ReadKey();
+        }
+
+        static async Task MainInternal(string[] args)
+        {
+            Header.Print();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    Out("Getting todays wallpaper info");
+                    var result = await client.GetAsync(HOST + ENDPOINT);
+                    Out("Received todays wallpaper info");
+                    var body = await result.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<RootObject>(body);
+                    if (data == null)
+                    {
+                        Out("The received answer is not valid");
+                        return;
+                    }
+                    var image = data.images.FirstOrDefault();
+                    if (image == null)
+                    {
+                        Out("The received answer contains no image information");
+                        return;
+                    }
+                    var imageurl = HOST + image.url;
+                    if (File.Exists(image.enddate + EXT))
+                    {
+                        Out("The image file has already been downloaded");
+                    }
+                    else
+                    {
+                        Out("Downloading image file");
+                        using (WebClient webClient = new WebClient())
+                        {
+                            await webClient.DownloadFileTaskAsync(imageurl, image.enddate + EXT);
+                        }
+                    }
+                    Out("Setting wallpaper");
+                    Wallpaper.Set(image.enddate + EXT, Wallpaper.Style.Stretched);
+                    Out("Wallpaper successfully applied");
+                }
+            }
+            catch (Exception ex)
+            {
+                Out("An error occurred: " + ex.Message);
+            }
+        }
+
+        private static void Out(string format, params string[] arg)
+        {
+            Console.WriteLine(format, arg);
+        }
+    }
+}
